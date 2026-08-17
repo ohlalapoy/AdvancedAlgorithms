29e591f99aab0e4107a6fb76d97235deddb4a66d
thanarat <ploythanarat@hotmail.co.uk>
2026-08-17 09:18:21 +0700

    add verify function

--- Linear_search.py
+++ Linear_search.py
@@ -17,4 +17,8 @@ def verify(index):
         print("Target found at the Index: ",index)
     else:
         print("Target not found in the list")
-        
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = linear_search(numbers, 6)
+verify(result)
