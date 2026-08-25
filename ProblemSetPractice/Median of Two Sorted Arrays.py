405175a5debe860ca0dd958f6925d10b1a5eedf2
thanarat <ploythanarat@hotmail.co.uk>
2026-08-25 07:30:08 +0700

    new Pset

---
+++ ProblemSetPractice/Median of Two Sorted Arrays.py
@@ -0,0 +1,15 @@
+import statistics as st
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        new_num = nums1+nums2
+        #print(new_num)
+        Num_all = sorted(new_num)
+
+        return st.median(Num_all)
+
+nums1 = [1,2]
+nums2 = [3,4]
+
+test = Solution()
+print(test.findMedianSortedArrays(nums1,nums2))
