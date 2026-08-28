8761d3410115b79c4db8b31c6563bae76efba86d
thanarat <ploythanarat@hotmail.co.uk>
2026-08-28 21:39:08 +0700

    add comment

--- ProblemSetPractice/Roman to Integer.py
+++ ProblemSetPractice/Roman to Integer.py
@@ -8,8 +8,10 @@ class Solution:
         sum = 0
         #new_list = list(s)
         #print(new_list)
-        for i in range(len(s)): # i in new_list
-            if i+1 < len(s) and symbol[s[i]] < symbol[s[i+1]]: #pass string เข้ามา
+        for i in range(len(s)): # วนลูปตัวอักษรเข้าไปเช็ค 
+            if i+1 < len(s) and symbol[s[i]] < symbol[s[i+1]]: 
+        # ถ้า i+1 < len คือเช็คว่ามีตัวถัดไปไหม และ พอเอาตัวอักษรใร List ไปเทียบกับ dic ด้านบน Key น้อยกว่าตัวถัดไปไหม
+        # ถ้าตัวหน้าดันค่าน้อยกว่าแล้วอยู่ข้างหน้า ก็ให้ลบออก เช่น XII X มากกว่า I จะไปตก else แต่บางเลข IX คือ 9 เลยต้องลบค่าออก
                 sum -= symbol[s[i]]
             else: 
                 sum += symbol[s[i]]
