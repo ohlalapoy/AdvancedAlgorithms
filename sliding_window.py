eb929d57b70bff1b6446f5a9abf818079bee4f15
thanarat <ploythanarat@hotmail.co.uk>
2026-09-13 06:19:24 +0700

    add pattern to the code

--- sliding_window.py
+++ sliding_window.py
@@ -7,6 +7,17 @@ Dynamic Window's problem
 
 '''
 
+#Template
+
+def sliding_window_fixed(input,window_size):
+    ans = window = input[0:window_size]
+    for right in range(window_size, len(input)):
+        left = right - window_size
+        remove input[left] from window
+        append input[right] to window
+        ans = optimal(ans,window)
+    return ans
+
 #Fixed Size Window - หาผลรวมสูงสุดของ subarray ขนาด k
 def max_sum_subarray(arr, k):
     n = len(arr)
