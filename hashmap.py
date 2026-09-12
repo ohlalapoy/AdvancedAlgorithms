500a4f89f1bc0b9d5e1b87e8f3876ba8779f47fc
thanarat <ploythanarat@hotmail.co.uk>
2026-09-12 10:34:40 +0700

    add details hasgmap

--- hashmap.py
+++ hashmap.py
@@ -1,8 +1,24 @@
-## Hash Map pattern ที่เห็นบ่อยๆ
-my_map = {}
+''' Hash Map pattern ที่เห็นบ่อยๆ
+ Hashable : Numbers, Strings and Tuples
+To track number that you''ve seen before !! 
+ Pattern for Hash 
+ - storing something
+ - Looking for something 
+ - Updating/initialise values
 
-for item in data:
+              Hash
+Key ─────────────────→ Index
+                         ↓
+                    ┌──────────┐
+                    │ Key Value│
+                    └──────────┘
+'''
+
+my_map = {} # สมุดเปล่า ไว้จดค่าที่เข้าไปเช็ค 
+data = []
+
+for item in data: 
     if item not in my_map:
-        my_map[item] = 1
+        my_map[item] = 1 
     else: 
-        my_map[item] += 1
+        my_map[item] += 1 
