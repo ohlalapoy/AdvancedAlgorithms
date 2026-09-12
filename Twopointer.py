3e8608f8a3cd0775e0df5f3ed1d28237ead23b7e
thanarat <ploythanarat@hotmail.co.uk>
2026-09-12 11:28:32 +0700

    add notes twopointer

---
+++ Twopointer.py
@@ -0,0 +1,13 @@
+'''
+Two pointers has 2 ways to solve
+- Same direction : starter and end start from the first index and secound index representively
+- opposite : starter started from first index and the secound one start from the end
+
+type of problem that you usually use Twopointer to solve
+- Palindromes
+- Reversals
+- Merging sorted data
+- "K" sized comparisons
+
+'''
+
