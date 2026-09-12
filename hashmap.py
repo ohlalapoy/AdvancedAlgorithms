867cf39c11680d8d07fced8f05aaff4273b2dea1
thanarat <ploythanarat@hotmail.co.uk>
2026-09-12 10:43:18 +0700

    Add comment

--- hashmap.py
+++ hashmap.py
@@ -1,4 +1,4 @@
-''' Hash Map pattern ที่เห็นบ่อยๆ
+''' Hash Map pattern ที่เห็นบ่อยๆ เป็นการเล่นกับ Key, values
  Hashable : Numbers, Strings and Tuples
 To track number that you''ve seen before !! 
  Pattern for Hash 
@@ -18,7 +18,7 @@ my_map = {} # สมุดเปล่า ไว้จดค่าที่เ�
 data = []
 
 for item in data: 
-    if item not in my_map:
-        my_map[item] = 1 
+    if item not in my_map: #check ว่าค่าที่เราต้องการหาอยู่ในนี้ไหม 
+        my_map[item] = 1 # ถ้าไม่มีให้เอาค่าเก็บเข้า my_map เพื่อให้รอบต่อไปเข้ามาเช็คต่อ
     else: 
-        my_map[item] += 1 
+        my_map[item] = my_map[item] + 1  #แต่ถ้าเจอ ให้บวกเพิ่มเข้าไป เหมือนเป้นการ count สะสมค่าที่เจอ
